8327b81a58dbcc40a5457a8eeb305f716abdef0e
Jerry W <jerrywiltse@gmail.com>
2017-09-07 17:23:33 -0400

    try fix appveyor

---
+++ build.py
@@ -0,0 +1,7 @@
+from conan.packager import ConanMultiPackager, os
+
+
+if __name__ == "__main__":
+    builder = ConanMultiPackager(username=os.getenv("CONAN_USERNAME"), channel=os.getenv("CONAN_CHANNEL"))
+    builder.add_common_builds()
+    builder.run()
