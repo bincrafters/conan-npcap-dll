b086b8781254693a0701c223d69235dca7890922
Jerry W <jerrywiltse@gmail.com>
2017-09-07 22:08:12 -0400

    try manual created builders

--- build.py
+++ build.py
@@ -3,9 +3,12 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds()
-    
-    for settings, options, env_vars, build_requires in builder.builds:
-        if settings["arch"] == "x86":
-            settings["arch"] = "Win32"
+    builder.add({"arch": "x86", "build_type": "Release"}, {"configuration": "Release No NetMon"})
+    builder.add({"arch": "x86", "build_type": "Debug"}, {"configuration": "Debug No NetMon"})
+    builder.add({"arch": "x86", "build_type": "Release"}, {"configuration": "Release No NetMon and AirPcap"})
+    builder.add({"arch": "x86", "build_type": "Debug"}, {"configuration": "Debug No NetMon and AirPcap"})
+    builder.add({"arch": "x86_64", "build_type": "Release"}, {"configuration": "Release No NetMon"})
+    builder.add({"arch": "x86_64", "build_type": "Debug"}, {"configuration": "Debug No NetMon"})
+    builder.add({"arch": "x86_64", "build_type": "Release"}, {"configuration": "Release No NetMon and AirPcap"})
+    builder.add({"arch": "x86_64", "build_type": "Debug"}, {"configuration": "Debug No NetMon and AirPcap"})
     builder.run()
