8bd2dc98ab45b8a546e59fb4faf530d6190bf3b2
Jerry W <jerrywiltse@gmail.com>
2017-09-21 23:20:54 -0400

    fixed test package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,21 +1,18 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 import os
 
 
-class TestPackage(ConanFile):
-    settings = "compiler", "build_type", "arch"
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
         
     def imports(self):
-        self.copy("*.lib", dst="bin", src="lib")
-        self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy("*.dll", dst="bin", src="bin")
-       
+        self.copy("*", dst="bin", src="lib")
+        
     def test(self):
         self.run(os.path.join("bin","test_package"))
-
